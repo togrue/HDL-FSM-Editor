326adda04ed4474115c822f58a3d8671713f3c73
Tobias Gruen <tobygruen@googlemail.com>
2025-07-19 05:41:50 +0200

    Rename modify_window_title to update_window_title

--- src/state_action_handling.py
+++ src/state_action_handling.py
@@ -47,7 +47,7 @@ class MyText:
         self.text = ""
         self.text_id.bind("<Control-z>", lambda event: self.text_id.undo())
         self.text_id.bind("<Control-Z>", lambda event: self.text_id.redo())
-        self.text_id.bind("<<TextModified>>", lambda event: undo_handling.modify_window_title())
+        self.text_id.bind("<<TextModified>>", lambda event: undo_handling.update_window_title())
         self.text_id.bind("<FocusIn>", lambda event: main_window.canvas.unbind_all("<Delete>"))
         self.text_id.bind(
             "<FocusOut>", lambda event: main_window.canvas.bind_all("<Delete>", lambda event: canvas_editing.delete())
