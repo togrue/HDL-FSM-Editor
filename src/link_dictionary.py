2c24db3eb5d9b4bfc2d0d23551df9c7a594687da
Tobias Gruen <tobygruen@googlemail.com>
2025-07-20 22:22:17 +0200

    Use GuiTab Internals

--- src/link_dictionary.py
+++ src/link_dictionary.py
@@ -58,7 +58,7 @@ class LinkDictionary:
         elif hdl_item_type == "custom_text_in_internals_tab":
             for text_line_number in range(1, number_of_lines + 1):
                 self.link_dict[file_name][file_line_number + text_line_number - 1] = {
-                    "tab_name": "Internals",
+                    "tab_name": GuiTab.INTERNALS,
                     "widget_reference": hdl_item_name,
                     "hdl_item_type": "",
                     "object_identifier": "",
