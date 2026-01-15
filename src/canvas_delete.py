3ca1a022651f7bd258c685038f5a49defcde918d
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-01-15 11:19:38 +0100

    A deleted element is also removed from its element dictionary

--- src/canvas_delete.py
+++ src/canvas_delete.py
@@ -10,8 +10,11 @@ import condition_action
 import custom_text
 import global_actions_clocked
 import global_actions_combinatorial
+import state
 import state_action
 import state_actions_default
+import state_comment
+import transition
 import undo_handling
 from project_manager import project_manager
 
@@ -52,13 +55,13 @@ class CanvasDelete:
         if item_type == "polygon":
             self._delete_reset_entry(tags_of_item_i)
         elif item_type == "window":
-            self._delete_window(tags_of_item_i)
+            self._delete_window(tags_of_item_i, canvas_id)
         elif item_type == "oval":
             self._delete_state(tags_of_item_i, canvas_id)
         elif item_type == "rectangle":
             self._delete_connector_or_transition(tags_of_item_i)
         elif item_type == "line":
-            self._delete_transition_line(tags_of_item_i)
+            self._delete_transition_line(tags_of_item_i, canvas_id)
         else:
             messagebox.showerror(
                 "Delete",
@@ -88,78 +91,83 @@ class CanvasDelete:
                 project_manager.reset_entry_button.config(state=tk.NORMAL)
                 self.design_was_changed = True
             elif tag.startswith("transition") and tag.endswith("_start"):  # transition<n>_start
-                transition = tag[0:-6]
-                transition_tags = self.canvas.gettags(transition)
+                transition_id = tag[0:-6]
+                transition_tags = self.canvas.gettags(transition_id)
                 for transition_tag in transition_tags:
                     if transition_tag.startswith("going_to_"):
-                        state = transition_tag[9:]
-                        self.canvas.dtag(state, transition + "_end")
+                        target_state = transition_tag[9:]
+                        self.canvas.dtag(target_state, transition_id + "_end")
                     elif transition_tag.startswith("ca_connection"):
                         condition_action_tag = transition_tag[:-4]
                         self.canvas.delete(condition_action_tag + "_anchor")
-                self.canvas.delete(transition)
-                self.canvas.delete(transition + "rectangle")  # delete priority rectangle
-                self.canvas.delete(transition + "priority")  # delete priority
+                self.canvas.delete(transition_id)
+                self.canvas.delete(transition_id + "rectangle")  # delete priority rectangle
+                self.canvas.delete(transition_id + "priority")  # delete priority
             elif tag == "polygon_for_move":
                 self.canvas.delete(tag)  # delete move polygon
 
-    def _delete_window(self, tags_of_item_i):
+    def _delete_window(self, tags_of_item_i, canvas_id):
         # Delete the window and all tags which refer to this window:
         for tag in tags_of_item_i:
-            item_id = self.canvas.find_withtag(tag)
             if tag.startswith("state_actions_default"):
-                ref = state_actions_default.StateActionsDefault.dictionary[item_id[0]]
+                ref = state_actions_default.StateActionsDefault.dictionary[canvas_id]
                 del custom_text.CustomText.read_variables_of_all_windows[ref.text_id]
                 del custom_text.CustomText.written_variables_of_all_windows[ref.text_id]
                 self.canvas.delete(tag)  # delete window
+                del state_actions_default.StateActionsDefault.dictionary[canvas_id]
                 project_manager.state_action_default_button.config(state=tk.NORMAL)
             elif tag.startswith("state_action"):
-                ref = state_action.StateAction.mytext_dict[item_id[0]]
+                ref = state_action.StateAction.mytext_dict[canvas_id]
                 del custom_text.CustomText.read_variables_of_all_windows[ref.text_id]
                 del custom_text.CustomText.written_variables_of_all_windows[ref.text_id]
                 self.canvas.delete(tag)  # delete window
+                del state_action.StateAction.mytext_dict[canvas_id]
             elif tag.endswith("_comment"):
                 self.canvas.delete(tag)  # delete window
                 self.canvas.delete(tag + "_line")  # delete line to window
                 self.canvas.dtag(
                     tag.replace("_comment", ""), tag + "_line_end"
                 )  # delete at state: "state"<integer>"_comment_line_end"
+                del state_comment.StateComment.dictionary[canvas_id]
             elif tag.startswith("condition_action"):
-                ref = condition_action.ConditionAction.dictionary[item_id[0]]
+                ref = condition_action.ConditionAction.dictionary[canvas_id]
                 del custom_text.CustomText.read_variables_of_all_windows[ref.condition_id]
                 del custom_text.CustomText.written_variables_of_all_windows[ref.condition_id]
                 del custom_text.CustomText.read_variables_of_all_windows[ref.action_id]
                 del custom_text.CustomText.written_variables_of_all_windows[ref.action_id]
                 self.canvas.delete(tag)  # delete window
+                del condition_action.ConditionAction.dictionary[canvas_id]
             elif tag == "global_actions1":
-                ref = global_actions_clocked.GlobalActionsClocked.dictionary[item_id[0]]
+                ref = global_actions_clocked.GlobalActionsClocked.dictionary[canvas_id]
                 del custom_text.CustomText.read_variables_of_all_windows[ref.text_before_id]
                 del custom_text.CustomText.written_variables_of_all_windows[ref.text_before_id]
                 del custom_text.CustomText.read_variables_of_all_windows[ref.text_after_id]
                 del custom_text.CustomText.written_variables_of_all_windows[ref.text_after_id]
                 self.canvas.delete(tag)  # delete window
+                del global_actions_clocked.GlobalActionsClocked.dictionary[canvas_id]
                 project_manager.global_action_clocked_button.config(state=tk.NORMAL)
             elif tag == "global_actions_combinatorial1":
-                ref = global_actions_combinatorial.GlobalActionsCombinatorial.dictionary[item_id[0]]
+                ref = global_actions_combinatorial.GlobalActionsCombinatorial.dictionary[canvas_id]
                 del custom_text.CustomText.read_variables_of_all_windows[ref.text_id]
                 del custom_text.CustomText.written_variables_of_all_windows[ref.text_id]
                 self.canvas.delete(tag)  # delete window
+                del global_actions_combinatorial.GlobalActionsCombinatorial.dictionary[canvas_id]
                 project_manager.global_action_combinatorial_button.config(state=tk.NORMAL)
             elif tag.startswith("connection"):  # connection<n>_start
                 connection = tag[0:-6]
                 connection_tags = self.canvas.gettags(connection)
                 for connection_tag in connection_tags:
                     if connection_tag.startswith("connected_to_state"):
-                        state = connection_tag[13:]  # delete tag "connection<n>_end" at state.
-                        self.canvas.dtag(state, tag[0:-6] + "_end")
+                        state_connected = connection_tag[13:]  # delete tag "connection<n>_end" at state.
+                        self.canvas.dtag(state_connected, tag[0:-6] + "_end")
                 self.canvas.delete(tag[0:-6])  # delete connection
             elif tag.startswith("ca_connection"):  # ca_connection<n>_anchor
                 ca_connection = tag[0:-7]
                 ca_connection_tags = self.canvas.gettags(ca_connection)
                 for ca_connection_tag in ca_connection_tags:
                     if ca_connection_tag.startswith("connected_to_transition"):
-                        transition = ca_connection_tag[13:]  # delete tag "ca_connection<n>_end" at state.
-                        self.canvas.dtag(transition, tag[0:-7] + "_end")
+                        transition_id = ca_connection_tag[13:]  # delete tag "ca_connection<n>_end" at state.
+                        self.canvas.dtag(transition_id, tag[0:-7] + "_end")
                 self.canvas.delete(tag[0:-7])  # delete connection
             self.design_was_changed = True
 
@@ -182,6 +190,7 @@ class CanvasDelete:
                 canvas_id_of_comment = tag[:-9]
                 self.canvas.delete(canvas_id_of_comment_line)
                 self.canvas.delete(canvas_id_of_comment)
+        del state.States.state_dict[canvas_id]
         self.design_was_changed = True
 
     def _delete_connector_or_transition(self, tags_of_item_i):
@@ -194,13 +203,15 @@ class CanvasDelete:
                 self._delete_starting_transition(tag)
         self.design_was_changed = True
 
-    def _delete_transition_line(self, tags_of_item_i):
+    def _delete_transition_line(self, tags_of_item_i, canvas_id):
+        transition_tag = None
         for tag in tags_of_item_i:
             if tag.startswith("transition"):
+                transition.TransitionLine.transition_dict.pop(canvas_id, None)
                 self.canvas.delete(tag)  # delete transition
                 self.canvas.delete(tag + "rectangle")  # delete priority rectangle
                 self.canvas.delete(tag + "priority")  # delete priority
-                transition = tag  # carries "transition<n>"
+                transition_tag = tag  # carries "transition<n>"
             elif tag.startswith("ca_connection"):  # Line to condition-action block
                 condition_action_tag = tag[:-4]
                 self.canvas.delete(condition_action_tag + "_anchor")
@@ -208,42 +219,47 @@ class CanvasDelete:
         for tag in tags_of_item_i:
             if tag.startswith("coming_from_"):
                 start_state = tag[12:]
-                self.canvas.dtag(start_state, transition + "_start")
+                self.canvas.dtag(start_state, transition_tag + "_start")
             elif tag.startswith("going_to_"):
                 end_state = tag[9:]
-                self.canvas.dtag(end_state, transition + "_end")
+                self.canvas.dtag(end_state, transition_tag + "_end")
         self._adapt_visibility_of_priority_rectangles_at_state(start_state)
         self.design_was_changed = True
 
     def _delete_ending_transition(self, tag):
-        transition = tag[0:-4]
-        transition_tags = self.canvas.gettags(transition)
+        transition_id = tag[0:-4]
+        transition_tags = self.canvas.gettags(transition_id)
         for transition_tag in transition_tags:
             if transition_tag.startswith("coming_from_"):
-                state = transition_tag[12:]
-                self.canvas.dtag(state, transition + "_start")
-                self._adapt_visibility_of_priority_rectangles_at_state(state)
+                state_id = transition_tag[12:]
+                transition.TransitionLine.transition_dict.pop(transition_id, None)
+                self.canvas.dtag(state_id, transition_id + "_start")
+                self._adapt_visibility_of_priority_rectangles_at_state(state_id)
             elif transition_tag.startswith("ca_connection"):
                 condition_action_tag = transition_tag[:-4]
+                condition_action_canvas_id = project_manager.canvas.find_withtag(condition_action_tag)[0]
+                condition_action.ConditionAction.dictionary.pop(condition_action_canvas_id, None)
                 self.canvas.delete(condition_action_tag + "_anchor")  # delete the window
                 self.canvas.delete(condition_action_tag)  # delete the line to the window
-        self.canvas.delete(transition)
-        self.canvas.delete(transition + "rectangle")  # delete priority rectangle
-        self.canvas.delete(transition + "priority")  # delete priority
+        self.canvas.delete(transition_id)
+        self.canvas.delete(transition_id + "rectangle")  # delete priority rectangle
+        self.canvas.delete(transition_id + "priority")  # delete priority
 
     def _delete_starting_transition(self, tag):
-        transition = tag[0:-6]
-        transition_tags = self.canvas.gettags(transition)
+        transition_id = tag[0:-6]
+        transition_tags = self.canvas.gettags(transition_id)
         for transition_tag in transition_tags:
             if transition_tag.startswith("going_to_"):
-                state = transition_tag[9:]
-                self.canvas.dtag(state, transition + "_end")
+                state_id = transition_tag[9:]
+                self.canvas.dtag(state_id, transition_id + "_end")
             elif transition_tag.startswith("ca_connection"):
                 condition_action_tag = transition_tag[:-4]
+                condition_action_canvas_id = project_manager.canvas.find_withtag(condition_action_tag)[0]
+                condition_action.ConditionAction.dictionary.pop(condition_action_canvas_id, None)
                 self.canvas.delete(condition_action_tag + "_anchor")
-        self.canvas.delete(transition)
-        self.canvas.delete(transition + "rectangle")  # delete priority rectangle
-        self.canvas.delete(transition + "priority")  # delete priority
+        self.canvas.delete(transition_id)
+        self.canvas.delete(transition_id + "rectangle")  # delete priority rectangle
+        self.canvas.delete(transition_id + "priority")  # delete priority
 
     def _adapt_visibility_of_priority_rectangles_at_state(self, start_state) -> None:
         tags_of_start_state = self.canvas.gettags(start_state)
