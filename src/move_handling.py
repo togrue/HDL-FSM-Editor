98768a6f973975ce325e077ab912cbecca7dbcad
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-01-19 18:19:30 +0100

    Removed not needed method

--- src/move_handling.py
+++ src/move_handling.py
@@ -74,42 +74,6 @@ def move_to_coordinates(event_x, event_y, move_list, first, move_to_grid):
             print("move: Fatal, unknown canvas type", "|" + item_type + "|")
 
 
-def _state_is_moved_to_near_to_state_or_connector(move_list, event_x, event_y) -> bool:
-    for entry in move_list:
-        moved_item_id = entry[0]
-        if project_manager.canvas.type(moved_item_id) == "oval":
-            ref = state.States.state_dict[moved_item_id]
-            # Keep the distance between event and anchor point constant:
-            event_x_mod, event_y_mod = event_x + ref.difference_x, event_y + ref.difference_y
-            event_x_mod = project_manager.state_radius * round(event_x_mod / project_manager.state_radius)
-            event_y_mod = project_manager.state_radius * round(event_y_mod / project_manager.state_radius)
-            state_coords = project_manager.canvas.coords(moved_item_id)
-            state_radius = (state_coords[2] - state_coords[0]) // 2
-            moved_state_coords = (
-                event_x_mod - state_radius,
-                event_y_mod - state_radius,
-                event_x_mod + state_radius,
-                event_y_mod + state_radius,
-            )
-            overlapping_list = project_manager.canvas.find_overlapping(
-                moved_state_coords[0] - project_manager.state_radius / 2,
-                moved_state_coords[1] - project_manager.state_radius / 2,
-                moved_state_coords[2] + project_manager.state_radius / 2,
-                moved_state_coords[3] + project_manager.state_radius / 2,
-            )
-            for overlapping_item in overlapping_list:
-                overlapping_with_connector = False
-                tags = project_manager.canvas.gettags(overlapping_item)
-                for tag in tags:
-                    if tag.startswith("connector"):
-                        overlapping_with_connector = True
-                if overlapping_item != moved_item_id and (
-                    project_manager.canvas.type(overlapping_item) == "oval" or overlapping_with_connector
-                ):
-                    return True
-    return False
-
-
 def _connector_moved_too_close_to_other_object(move_list, event_x, event_y) -> bool:
     for entry in move_list:
         moved_item_id = entry[0]
